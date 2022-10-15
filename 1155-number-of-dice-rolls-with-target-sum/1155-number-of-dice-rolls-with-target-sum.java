88e71377ffecf2214518992ad9700e16c1678024
manojnjaya1 <88700488+manojnjaya1@users.noreply.github.com>
2022-10-15 23:31:25 +0530

    Time: 52 ms (29.45%), Space: 42.5 MB (51.41%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.java
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.java
@@ -1,9 +1,20 @@
 class Solution {
-    public int numRollsToTarget(int n, int k, int target) {
-        int[][] dp=new int[n+1][target+1];
-        
-    for(int[] row:dp)Arrays.fill(row,-1);
-      return solve(n,k,target,dp)  ;
+    public int numRollsToTarget(int n, int k, int t) {
+        int[][] dp=new int[n+1][t+1];
+        dp[0][0]=1;
+        for(int dice=1;dice<=n;dice++){
+            for(int target=1;target<=t;target++){
+                int ans=0;
+        for(int i=1;i<=k;i++){
+            if(target-i>=0)
+            ans=(ans+dp[dice-1][target-i])%1000000007;
+        }
+                dp[dice][target] =ans;
+            }
+        }
+        return dp[n][t];
+   // for(int[] row:dp)Arrays.fill(row,-1);
+     // return solve(n,k,target,dp)  ;
     }
     int solve(int dice,int faces, int target,int[][] dp){
         if(dice==0 && target!=0)return 0;
