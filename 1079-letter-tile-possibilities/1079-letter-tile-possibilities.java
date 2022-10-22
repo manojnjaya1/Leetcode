02e8e73f07261d4c786a330106e730e075096666
manojnjaya1 <88700488+manojnjaya1@users.noreply.github.com>
2022-10-22 18:35:29 +0530

    Time: 3 ms (96.33%), Space: 42.6 MB (60.88%) - LeetHub

---
+++ 1079-letter-tile-possibilities/1079-letter-tile-possibilities.java
@@ -0,0 +1,28 @@
+class Solution {
+    Map<Integer, Integer> memo = new HashMap<>();
+    
+    public int numTilePossibilities(String tiles) {
+        int[] count = new int[26];
+        for (char c : tiles.toCharArray()) count[c - 'A']++;
+        return dfs(count);
+    }
+    
+    int dfs(int[] arr) {
+        int key = Arrays.hashCode(arr);
+        if (memo.containsKey(key)) {
+            return memo.get(key);
+        }
+        
+        int sum = 0;
+        for (int i = 0; i < 26; i++) {
+            if (arr[i] == 0) continue;
+            sum++;
+            arr[i]--;
+            sum += dfs(arr);
+            arr[i]++;
+        }
+        
+        memo.put(key, sum);
+        return sum;
+    }
+}
