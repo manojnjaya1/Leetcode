157fd8d3db4978bf11f073b8804eb8cb5832a14c
manojnjaya1 <88700488+manojnjaya1@users.noreply.github.com>
2022-10-26 13:17:56 +0530

    Time: 11 ms (57.57%), Space: 44.9 MB (5.30%) - LeetHub

---
+++ 1208-get-equal-substrings-within-budget/1208-get-equal-substrings-within-budget.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int equalSubstring(String s, String t, int k) {
+        int n=s.length();
+        int i=0;
+        int j=0;
+        for(j=0;j<n;j++){
+            k-=Math.abs(s.charAt(j)-t.charAt(j));
+            if(k<0){
+                k+=Math.abs(s.charAt(i)-t.charAt(i));
+                i++;
+            }
+        }
+        return j-i;
+    }
+}
