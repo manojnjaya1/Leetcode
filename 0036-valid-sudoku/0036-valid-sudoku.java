4acd883cc7cd72a94a7227eae753983c019b38c5
manojnjaya1 <88700488+manojnjaya1@users.noreply.github.com>
2022-11-23 17:52:33 +0530

    Time: 23 ms (30.67%), Space: 47.8 MB (37.81%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+         HashSet set=new HashSet();
+         for(int i=0;i<board.length;i++){
+            for(int j=0;j<board[0].length;j++){
+                if(board[i][j]!='.'){
+                    if(!set.add("row"+i+board[i][j]) || !set.add("col"+j+board[i][j]))
+                        return false;
+                    
+                    if(!set.add("box"+((i/3)*3 +j/3)+board[i][j]))return false;
+                }
+                
+                
+                
+                
+            }
+         }
+        
+        
+        
+      return true;
+        
+    }
+}
