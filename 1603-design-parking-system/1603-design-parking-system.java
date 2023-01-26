3a01ba2d77869d2b83602be02b8b12985786dcf6
manojnjaya1 <88700488+manojnjaya1@users.noreply.github.com>
2023-01-26 20:16:48 +0530

    Time: 6 ms (100.00%), Space: 43 MB (54.80%) - LeetHub

--- 1603-design-parking-system/1603-design-parking-system.java
+++ 1603-design-parking-system/1603-design-parking-system.java
@@ -1,39 +1,11 @@
 class ParkingSystem {
-     int Big=0;
-    int Medium=0;
-    int Small=0;
+  int[] count;
     public ParkingSystem(int big, int medium, int small) {
-        Big=big;
-        Medium=medium;
-        Small=small;
+        count =new int[]{big,medium,small};
     }
     
     public boolean addCar(int carType) {
-        if(carType==1 ){
-            if(Big>=1){
-            Big--;
-            return true;
-            }
-            else 
-                return false;
-            }
-          if(carType==2 ){
-            if(Medium>=1){
-            Medium--;
-            return true;
-            }
-            else 
-                return false;
-            }
-          if(carType==3 ){
-            if(Small>=1){
-            Small--;
-            return true;
-            }
-            else 
-                return false;
-            }
-       return false;
+        return count[carType-1]-->0;
     }
 }
 
