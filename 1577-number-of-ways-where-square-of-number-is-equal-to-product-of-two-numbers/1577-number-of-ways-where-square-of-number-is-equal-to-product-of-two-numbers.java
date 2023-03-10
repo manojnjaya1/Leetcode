d3b6707923080c04f9a71e84369888e94ff2e80d
manojnjaya1 <88700488+manojnjaya1@users.noreply.github.com>
2023-03-10 09:17:32 +0530

    Time: 48 ms (71.62%), Space: 42.5 MB (44.59%) - LeetHub

--- 1577-number-of-ways-where-square-of-number-is-equal-to-product-of-two-numbers/1577-number-of-ways-where-square-of-number-is-equal-to-product-of-two-numbers.java
+++ 1577-number-of-ways-where-square-of-number-is-equal-to-product-of-two-numbers/1577-number-of-ways-where-square-of-number-is-equal-to-product-of-two-numbers.java
@@ -54,7 +54,7 @@ class Solution {
         for (int i=0; i<n1; i++) {
             map.put((long)nums1[i]*nums1[i], map.getOrDefault((long)nums1[i]*nums1[i], 0) + 1);
         }
-        for (int j=0; j<n2; j++) {
+        for (int j=0; j<n2-1; j++) {
             for (int k=j+1; k<n2; k++) {
                 result += (long)map.getOrDefault((long)nums2[j]*nums2[k], 0);
             }
